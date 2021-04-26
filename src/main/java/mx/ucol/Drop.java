b4a115812110f23a9ce7543a74acff9c52c3f4da
pakkofrank <jpalma@ucol.mx>
2021-04-25 23:08:58 -0500

    creo que ya

--- src/main/java/mx/ucol/Drop.java
+++ src/main/java/mx/ucol/Drop.java
@@ -1,8 +1,10 @@
 package mx.ucol;
 
 public class Drop {
-    private String message;
+    private String message []  = new String[10];
     private boolean empty = true;
+    private boolean fully = false;
+    private int pos = 0;
 
     public synchronized String take() {
         while (empty) {
@@ -13,21 +15,32 @@ public class Drop {
             }
         }
 
-        empty = true;
-        notifyAll();
+        String res = this.message[this.pos - 1];
+        --this.pos;
+        if (this.pos == 0) {
+            this.empty = true;
+        }
 
-        return message;
+        this.fully = false;
+        this.notifyAll();
+        return res;
+
     }
 
     public synchronized void put(String message) {
-        while (!empty) {
+        while (this.fully) {
             try {
                 wait();
             } catch (InterruptedException e) {}
         }
 
+        this.message[this.pos] = message;
+        ++this.pos;
+        if (this.pos == 9) {
+            this.fully = true;
+        }
+
         empty = false;
-        this.message = message;
         notifyAll();
     }
 }
